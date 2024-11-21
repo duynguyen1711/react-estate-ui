44a0f4f945de4b0b7ae7c6fcd3cb1a7baa3cf156
duynguyen1711 <duynguyenn1711@gmail.com>
2024-11-21 22:46:57 +0700

    done slider & format code

--- src/components/pin/Pin.jsx
+++ src/components/pin/Pin.jsx
@@ -1,24 +1,24 @@
-import { Marker, Popup } from "react-leaflet";
+import { Marker, Popup } from 'react-leaflet';
 
-import "./pin.scss";
-import { Link } from "react-router-dom";
+import './pin.scss';
+import { Link } from 'react-router-dom';
 
-const Pin = ({ item }) => {
+const Pin = ({ item, isListPage }) => {
+  return (
+    <Marker position={[item.latitude, item.longitude]}>
+      <Popup>
+        <div className='popupContainer'>
+          {isListPage && <img src={item.img} alt='' />}
 
-    return (
-        <Marker position={[item.latitude, item.longitude]}>
-            <Popup>
-                <div className="popupContainer">
-                    <img src={item.img} alt="" />
-                    <div className="textContainer">
-                        <Link to={`/${item.id}`}>{item.title}</Link>
-                        <span>{item.bedroom} bedroom</span>
-                        <b>$ {item.price}</b>
-                    </div>
-                </div>
-            </Popup>
-        </Marker>
-    )
-}
+          <div className='textContainer'>
+            <Link to={`/${item.id}`}>{item.title}</Link>
+            <span>{item.bedroom} bedroom</span>
+            <b>$ {item.price}</b>
+          </div>
+        </div>
+      </Popup>
+    </Marker>
+  );
+};
 
-export default Pin
+export default Pin;
--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,29 +1,68 @@
-import "./slider.scss";
+import { useEffect, useState } from 'react';
+import './slider.scss';
 
 const Slider = ({ images }) => {
-    return (
-        <div className="slider">
-            <div className="fullSlider">
-                <div className="arrow">
-                    <img src="/arrow.png" />
-                </div>
-                <div className="imgContainer"></div>
-                <div className="arrow">
-                    <img src="/arrow.png" />
-                </div>
-            </div>
+  const [imageIndex, setImageIndex] = useState(null);
+  const changSlide = (direction) => {
+    if (direction === 'left') {
+      if (imageIndex === 0) {
+        setImageIndex(images.length - 1);
+      } else {
+        setImageIndex(imageIndex - 1);
+      }
+    } else {
+      if (imageIndex === images.length - 1) {
+        setImageIndex(0);
+      } else {
+        setImageIndex(imageIndex + 1);
+      }
+    }
+  };
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'Escape') {
+        setImageIndex(null); // Đóng slider
+      }
+    };
 
-            <div className="bigImage">
-                <img src={images[0]} />
-            </div>
-            <div className="smallImages">
-                {images.slice(1).map((image, index) => (
-                    <img src={image} key={index} />
-                )
-                )}
-            </div>
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown); // Dọn dẹp sự kiện
+    };
+  }, []);
+  return (
+    <div className='slider'>
+      {imageIndex !== null && (
+        <div className='fullSlider'>
+          <div className='arrow' onClick={() => changSlide('left')}>
+            <img src='/arrow.png' />
+          </div>
+          <div className='imgContainer'>
+            <img src={images[imageIndex]} />
+          </div>
+          <div className='arrow' onClick={() => changSlide('right')}>
+            <img src='/arrow.png' className='right' />
+          </div>
+          <div className='close' onClick={() => setImageIndex(null)}>
+            X
+          </div>
         </div>
-    )
-}
+      )}
 
-export default Slider
+      <div className='bigImage'>
+        <img src={images[0]} onClick={() => setImageIndex(0)} />
+      </div>
+      <div className='smallImages'>
+        {images.slice(1).map((image, index) => (
+          <img
+            src={image}
+            key={index}
+            onClick={() => setImageIndex(index + 1)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
